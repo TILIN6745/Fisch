60dc7eb63a8e02b0859af32781fe8e57e8827260
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-10-17 17:27:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -55,13 +55,13 @@ async function start(file) {
   if (isRunning) return;
   isRunning = true;
 
-  say('The Mystic\nBot', {
+  say('MysticMOD', {
     font: 'chrome',
     align: 'center',
     gradient: ['red', 'magenta'],
   });
 
-  say(`Bot creado por Bruno Sobrino`, {
+  say(`Bot Comunitario\nGrupos de Ayuda en bit.ly/MSOS`, {
     font: 'console',
     align: 'center',
     gradient: ['red', 'magenta'],
