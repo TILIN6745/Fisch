e8b848d86acd3566de116395ceec5c775881051b
Tilinn <jm293229@gmail.com>
2024-11-09 10:02:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ const connectionOptions = {
     logger: Pino({ level: 'silent' }),
     printQRInTerminal: opcion === '1' || methodCodeQR,
     mobile: MethodMobile,
-    browser: opcion === '1' ? ['MysticMOD', 'Safari', '2.0.0'] : methodCodeQR ? ['MysticMOD', 'Safari', '2.0.0'] : ['Ubuntu', 'Chrome', '20.0.04'],
+    browser: opcion === '1' ? ['Fish', 'Safari', '2.0.0'] : methodCodeQR ? ['Fish', 'Safari', '2.0.0'] : ['Ubuntu', 'Chrome', '20.0.04'],
     auth: {
         creds: state.creds,
         keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: 'fatal' }).child({ level: 'fatal' })),
