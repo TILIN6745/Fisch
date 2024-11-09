8dbb23d28890eec5db82a8b7b6b3f9826e999f4e
Tilinn <jm293229@gmail.com>
2024-11-09 09:51:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function start(file) {
   if (isRunning) return;
   isRunning = true;
 
-  say('MysticMOD', {
+  say('FISH BOT', {
     font: 'chrome',
     align: 'center',
     gradient: ['red', 'magenta'],
